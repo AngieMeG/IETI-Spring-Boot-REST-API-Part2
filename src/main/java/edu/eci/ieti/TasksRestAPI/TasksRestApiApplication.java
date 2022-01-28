5e9adc56505bed492568e0118fc5975bddff8e83
Angie Medina <angie.medina@mail.escuelaing.edu.co>
2022-01-28 18:59:49 -0500

    Spring initializr

---
+++ src/main/java/edu/eci/ieti/TasksRestAPI/TasksRestApiApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.TasksRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/ieti/TasksRestAPI/TasksRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.TasksRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
